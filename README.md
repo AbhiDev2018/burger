ae8d893a01dcc8d1a2f050d1fe95dcee4ff6a0e4
Rikina Pradhan <abhishekrp.info@gmail.com>
2018-10-17 18:25:39 -0400

    burger

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # burger
+The application allows a user to add a burger that will be initially displayed in the "Not Devoured" section. Then when one clicks on the "Devour" button, the entry shows up in the "Already Devoured"section! </br> 
+Once the burger is 'devoured' the user can click the Delete button to delete/remove that entry. This will delete that burger </br>
+from the underlying db as well.</br>
+
+Even though I have not deployed to Heroku, I have used the JAWSDB_URL environment variable for db connection details. For locally hosted version, environment variable is "MYSQL_PWD" and stored in an .env file. The .env file type is in .gitignore file. When testing/executing this app locally you need to have your own .env file and provide the value for MYSQL_PWD variable in it.</br>
+
---
+++ config/connection.js
@@ -0,0 +1,35 @@
+//Require mysql npm package
+var mysql = require("mysql");
+
+//Require dotenv package.
+require("dotenv").config();
+
+//***I'm adding this but not pushing code to Heroku
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+  //else use local db.
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PWD,
+    database: "burger_db"
+  });
+}
+
+connection.connect(function(err) {
+  //If there is an error when connecting to the database, log the error to the console.
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  //If a database connection is established, log the database thread number.
+  console.log("connected as id " + connection.threadId);
+});
+
+//Export the connection properties so that we can use them in other files.
+module.exports = connection;
